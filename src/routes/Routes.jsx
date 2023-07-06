f5a888e13e358f71188fe31fc06381b892c6906a
MushfikYeasirPk <pkm2243@mail.com>
2023-07-06 13:43:29 +0600

    update route

--- src/routes/Routes.jsx
+++ src/routes/Routes.jsx
@@ -13,6 +13,7 @@ import MyEnrolledClasses from "../pages/Dashboard/My Enrolled Class/MyEnrolledCl
 import ManagerUsers from "../pages/Dashboard/ManagerUsers/ManagerUsers";
 import ManageClasses from "../pages/Dashboard/Manage Classes/ManageClasses";
 import Payment from "../pages/Dashboard/Payment/Payment";
+import About from "../Components/About/About";
 
 
 const router = createBrowserRouter([
@@ -34,10 +35,10 @@ const router = createBrowserRouter([
         path: "/register",
         element: <Register></Register>
       },
-      // {
-      //   path: "/dashboard",
-      //   element: <Dashboard></Dashboard>
-      // },
+      {
+        path: "/about",
+        element: <About></About>
+      },
       {
         path: "/instructors",
         element: <Instructors></Instructors>
