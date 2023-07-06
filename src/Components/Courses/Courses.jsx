a939235314bc58e1b66322da7d5a3f9484d83828
MushfikYeasirPk <pkm2243@mail.com>
2023-07-06 13:34:44 +0600

    courses comp

---
+++ src/Components/Courses/Courses.jsx
@@ -0,0 +1,19 @@
+
+import React  from "react";
+
+
+
+const Courses = () => {
+	
+
+	return (
+		<div className='container mx-auto px-4 md:px-11  '>
+			<h1 className='text-center text-3xl uppercase font-semibold mt-14 md:mb-9 mb-5 text-red-500'>
+				Popular Courses
+			</h1>
+			
+		</div>
+	);
+};
+
+export default Courses;
