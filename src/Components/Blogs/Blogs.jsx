2f5f6df02633df9ec9842af6dfa1cc05f6e61e09
MushfikYeasirPk <pkm2243@mail.com>
2023-07-06 13:33:29 +0600

    blog comp

---
+++ src/Components/Blogs/Blogs.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+import { Link } from "react-router-dom";
+
+
+const Blogs = () => {
+	
+
+	return (
+		<div className='container mx-auto my-24  px-4 md:px-11  '>
+			<h1 className='text-center text-3xl uppercase font-semibold mt-9 md:mb-9 mb-5 text-red-500'>
+				Latest Blog & News
+			</h1>
+			
+		</div>
+	);
+};
+
+export default Blogs;
