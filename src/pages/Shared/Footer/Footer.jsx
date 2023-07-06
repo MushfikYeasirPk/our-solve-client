11529af5521f6065b5796ceea3d7ca7bc5df29b4
MushfikYeasirPk <pkm2243@mail.com>
2023-07-06 12:32:56 +0600

    set routes and provider

---
+++ src/pages/Shared/Footer/Footer.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+const Footer = () => {
+  return (
+    <div>
+      <footer className="footer p-2 flex justify-center text-center bg-slate-800 ">
+       
+        <div className='text-center pb-2 text-xl bg-slate-800'>
+          <p>Copyright © 2023 - All right reserved by VumPros Industries Ltd</p>
+        </div>
+
+      </footer>
+
+    </div>
+  );
+};
+
+export default Footer;
---
+++ src/routes/Routes.jsx
@@ -0,0 +1,90 @@
+import { createBrowserRouter } from "react-router-dom";
+import Register from "../pages/Register/Register";
+import SignIn from "../pages/SignIn/SignIn";
+import Home from "../pages/Home/Home";
+import Main from "../Layout/Main";
+import ErrorPage from "../pages/ErrorPage/ErrorPage";
+import Dashboard from "../pages/Dashboard/Dashboard";
+import Instructors from "../pages/Instructors/Instructors";
+import Classes from "../pages/Classes/Classes";
+import Mycart from "../pages/Dashboard/Mycart/Mycart";
+import MySelectedClass from "../pages/Dashboard/My Selected Classes/MySelectedClass";
+import MyEnrolledClasses from "../pages/Dashboard/My Enrolled Class/MyEnrolledClasses";
+import ManagerUsers from "../pages/Dashboard/ManagerUsers/ManagerUsers";
+import ManageClasses from "../pages/Dashboard/Manage Classes/ManageClasses";
+import Payment from "../pages/Dashboard/Payment/Payment";
+
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Main></Main>,
+    errorElement: <ErrorPage></ErrorPage>,
+    children: [
+      {
+        path: "/",
+        element: <Home></Home>
+      },
+
+      {
+        path: "/signIn",
+        element: <SignIn></SignIn>
+      },
+      {
+        path: "/register",
+        element: <Register></Register>
+      },
+      // {
+      //   path: "/dashboard",
+      //   element: <Dashboard></Dashboard>
+      // },
+      {
+        path: "/instructors",
+        element: <Instructors></Instructors>
+      },
+      {
+        path: "/classes",
+        element: <Classes></Classes>
+      }
+    ]
+
+
+  },
+  {
+    path: "dashboard",
+    element: <Dashboard></Dashboard>,
+    errorElement: <ErrorPage></ErrorPage>,
+    children: [
+      {
+        path: "mycart",
+        element: <Mycart></Mycart>
+      },
+      {
+        path: "mycart/myselectedclasses",
+        element: <MySelectedClass></MySelectedClass>
+      },
+      {
+        path: "mycart/myenrolledclasses",
+        element: <MyEnrolledClasses></MyEnrolledClasses>
+      },
+      {
+        path: "mycart/manageusers",
+        element: <ManagerUsers></ManagerUsers>
+      },
+      {
+        path: "mycart/msnsgeclasses",
+        element: <ManageClasses></ManageClasses>
+      },
+      {
+        path: "mycart/myselectedclasses/payment",
+        element: <Payment></Payment>
+      }
+
+
+    ]
+
+  }
+
+]);
+
+export default router;
