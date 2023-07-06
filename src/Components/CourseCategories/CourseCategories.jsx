d9b5ca5c609660a34c578a817440ac8805bc13e2
MushfikYeasirPk <pkm2243@mail.com>
2023-07-06 13:34:30 +0600

    course catagories comp

---
+++ src/Components/CourseCategories/CourseCategories.jsx
@@ -0,0 +1,21 @@
+import React, { useEffect } from "react";
+
+import "./CourseCategories.css";
+
+
+import { Link } from "react-router-dom";
+
+const CourseCategories = () => {
+
+
+  return (
+    <div className="container mx-auto px-4 md:px-11">
+      <h1 className="text-center text-3xl uppercase font-semibold mt-24 md:mb-9 mb-5 text-red-500">
+        Choose Course Categories
+      </h1>
+     
+    </div>
+  );
+};
+
+export default CourseCategories;
