ae8366b20fe9f6f0107a2757db0de787328bc8c2
MushfikYeasirPk <pkm2243@mail.com>
2023-07-06 16:06:55 +0600

    setup resister page

--- src/pages/Register/Register.jsx
+++ src/pages/Register/Register.jsx
@@ -109,12 +109,20 @@ const Register = () => {
 
 
     return (
-        <div className='flex items-center justify-center mt-5 mb-5'>
+        <div className='container mx-auto px-4 mb-20 md:px-11'>
+            <div className="grid place-items-center h-screen">
+        <div className="md:w-2/4 w-full bg-white p-5 drop-shadow-xl ">
+          <div className='flex items-center justify-center'>
+          <svg className="text-2xl " xmlns="http://www.w3.org/2000/svg" height="1em" viewBox="0 0 448 512"><path d="M304 128a80 80 0 1 0 -160 0 80 80 0 1 0 160 0zM96 128a128 128 0 1 1 256 0A128 128 0 1 1 96 128zM49.3 464H398.7c-8.9-63.3-63.3-112-129-112H178.3c-65.7 0-120.1 48.7-129 112zM0 482.3C0 383.8 79.8 304 178.3 304h91.4C368.2 304 448 383.8 448 482.3c0 16.4-13.3 29.7-29.7 29.7H29.7C13.3 512 0 498.7 0 482.3z"/></svg>
+          </div>
+          <h3 className="mt-3 text-center text-3xl font-semibold text-gray-700">SignUp</h3>
             <div className="flex items-center justify-center">
                 <div className="m-auto w-96">
+                </div>
+                </div>
 
-                    <form onSubmit={handleSubmit(onSubmit)} className="bg-white rounded-lg px-8 py-10 mb-4">
-                        <div className="mb-4">
+                    <form onSubmit={handleSubmit(onSubmit)} className=" rounded-lg px-8  mb-4">
+                        <div className="mb-4 ">
                             <label className="block text-gray-700 font-bold mb-2" htmlFor="useremail">
                                 User Name
                             </label>
@@ -186,17 +194,14 @@ const Register = () => {
                         </div>
 
                     </form>
-                    <p className="text-center text-lg">
+                    <p className="text-center text-black  text-lg">
                         Already have an account?{' '}
                         <Link to='/signIn' className="font-bold hover:underline" href="#">
                             Sign in
                         </Link>
                     </p>
                 </div>
-                <div className="text-center lg:text-left px-10">
-                    <h1 className="text-5xl font-bold">Sign up now!</h1>
-                    <p className="text-5xl font-bold">Save Toy!</p>
-                </div>
+                
             </div>
         </div>
     );
